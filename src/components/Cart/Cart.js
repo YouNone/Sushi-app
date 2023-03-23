da95955c7a5d85b1792bfe6645d43536336184f2
Alex <velengura.alex@gmail.com>
2023-03-23 09:50:18 +0200

    adding same items in cart fix

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,12 +6,17 @@ import CartItem from "./CartItem/CartItem";
 
 const Cart = (props) => {
   const cartContext = useContext(CartContext);
-  const totalAmount = `$${cartContext.totalAmount.toFixed(2)}`;
+
+  const totalAmount = `$${Math.abs(cartContext.totalAmount).toFixed(2)}`;
   const hasItems = cartContext.items.length > 0;
 
-  const removeCartHandler = (id) => {};
+  const removeCartItemHandler = (id) => {
+    cartContext.removeItem(id);
+  };
 
-  const addCartItemHandler = (item) => {};
+  const addCartItemHandler = (item) => {
+    cartContext.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
@@ -19,28 +24,30 @@ const Cart = (props) => {
         <CartItem
           key={item.id}
           name={item.name}
-          price={item.price}
           amount={item.amount}
-          onAdd={addCartItemHandler(null, item)}
-          onRemove={removeCartHandler.bind(null, item.id)}
+          price={item.price}
+          onAdd={addCartItemHandler.bind(null, item)}
+          onRemove={removeCartItemHandler.bind(null, item.id)}
         />
       ))}
     </ul>
   );
+
   return (
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
-        <span>Total</span>
+        <span>Итого</span>
         <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={props.onHideCart}>
-          Close
+          Закрыть
         </button>
-        {hasItems && <button className={styles.button}>Order</button>}
+        {hasItems && <button className={styles.button}>Заказать</button>}
       </div>
     </Modal>
   );
 };
+
 export default Cart;
