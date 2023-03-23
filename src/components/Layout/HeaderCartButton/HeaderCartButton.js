c3b4b2f88a2a2eab605ef29ac40490d70eebc00e
Alex <velengura.alex@gmail.com>
2023-03-23 10:17:25 +0200

    styles added to button

--- src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,16 +1,31 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../../../assets/CartIcon.js";
 import CartContext from "../../../store/CartContext.js";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartContext = useContext(CartContext);
+  const [isButtonAnimated, setButtonAnimation] = useState(false);
   const cartItemsCount = cartContext.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
 
+  const buttonClasses = `${styles.button} ${
+    isButtonAnimated ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+    setButtonAnimation(true);
+    setTimeout(() => {
+      setButtonAnimation(false);
+    }, 300);
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
